1dcb9a62da31ff28c29c5aa881a2f27d87ded500
Alee1234em <133707385+Alee1234em@users.noreply.github.com>
2023-05-15 16:17:15 -0400

    primera subida
    kapshdbnlk

---
+++ prueba2/script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById("form")
+const btn = document.querySelector(".btn-login")
+function login(user,password){
+    if (user == "ale" && password == "1234"){
+        window.location="index2.html"
+    }else{
+        alert("Usuario no encontrado")
+    }
+}
+
+btn.addEventListener("click",e=>{
+    e.preventDefault()
+    let user = document.getElementById("nombre")
+    let password = document.getElementById("pass");
+
+
+    login(user.value,password.value)
+})
